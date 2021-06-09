984a8809fce04e4af96fced741875140137425e2
av-dev2 <av-dev2@aakvatech.com>
2021-06-09 23:03:06 +0300

    AV Fixtures Automation

--- csf_tz/csf_tz/doctype/av_fixture/av_fixture.py
+++ csf_tz/csf_tz/doctype/av_fixture/av_fixture.py
@@ -8,23 +8,28 @@ from frappe.model.document import Document
 
 
 class AVFixture(Document):
-    def create_fixture():
-        #frappe.msgprint("validate")
-        for identified_app in frappe.get_installed_apps():
-            for hook in frappe.get_hooks("fixtures", app_name=identified_app):
-                filters = None
-                if isinstance(hook, dict):
-                    custom_doctype = hook.get("doctype")
-                    filters = hook.get("filters")
-                    for filter in filters:
-                        for identified_filter in list(filter[2]):
-                            required_doctype = identified_filter.split("-")[0]
-                            """
-                            doc=frappe.new_doc("AV Fixture")
-                            doc.custom_doctype = custom_doctype
-                            doc.identified_filter = identified_filter
-                            doc.identified_app = identified_app
-                            doc.required_doctype = required_doctype
-                            doc.insert()"""
-    #create_fixture()
+    pass
 
+
+def create_fixture():
+    #frappe.db.sql("DELETE FROM `tabAuFix` WHERE name != 'a1' ")
+    frappe.db.commit()
+    for app_name in frappe.get_installed_apps():
+        for fixture_doc in frappe.get_hooks("fixtures", app_name=app_name):
+            filters = None
+            if isinstance(fixture_doc, dict):
+                custom_doctype = fixture_doc.get("doctype")
+                filters = fixture_doc.get("filters")
+                for filter in filters:
+                    for identified_filter in list(filter[2]):
+                        required_doctype = identified_filter.split("-")[0]
+
+                        doc = frappe.new_doc("AV Fixture")
+                        doc.custom_doctype = custom_doctype
+                        doc.identified_filter = identified_filter
+                        doc.identified_app = app_name
+                        doc.required_doctype = required_doctype
+                        doc.insert()
+
+
+create_fixture()
