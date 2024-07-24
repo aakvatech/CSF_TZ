21fce8079767bd91861dd82db21203e50f7558d8
iphenelist <innocentphenelist@gmail.com>
2024-07-24 18:22:41 +0300

    feat: landed cost ite automation

--- csf_tz/hooks.py
+++ csf_tz/hooks.py
@@ -66,7 +66,8 @@ doctype_js = {
     "Salary Slip": "csf_tz/salary_slip.js",
     "Landed Cost Voucher": "csf_tz/landed_cost_voucher.js",
     "Additional Salary": "csf_tz/additional_salary.js",
-    "BOM": "csf_tz/bom_addittional_costs.js"
+    "BOM": "csf_tz/bom_addittional_costs.js",
+    "Landed Cost Voucher": "csf_tz/landed_cost_item.js"
 }
 doctype_list_js = {
     "Custom Field": "csf_tz/custom_field.js",
